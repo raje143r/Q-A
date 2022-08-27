890642fbd121ded1330c302ae1d67b57226d3512
raje143r <raje143r@gmail.com>
2022-08-27 22:45:12 +0530

    Update DeployScript.groovy

--- DeployScript.groovy
+++ DeployScript.groovy
@@ -8,7 +8,7 @@ pipeline{
                     def gender = "${params.project}"
                         def chk = "${project}"
                         def chk1= "${project}"
-                 input message: "${params.project}"
+                 input message: 'projectname : ${params.project}  archivename : ${params.archive}'
                          ok: 'Release!'
                 }
                 }
