8444c48158a0c7d844137be66898d62b8d5212ee
raje143r <raje143r@gmail.com>
2022-09-07 14:58:45 +0530

    Create testchk1.groovy

---
+++ testchk1.groovy
@@ -0,0 +1,19 @@
+pipeline{
+       agent any
+	     options { 
+		     timestamps() 
+		     
+	     }
+        stages{
+              stage("build"){
+					   steps{
+					   script {
+							def workspace = pwd()
+					  }
+						echo "${workspace}"
+						}
+					   }
+			   }
+        
+     
+}
